cfaff5a17cbf174f313555f2e97b3b4a67bbf613
FilipiLira <filipe19.fc@gmail.com>
2020-06-15 22:32:33 -0300

    15/06/2020

--- app/Http/Services/ProductRepository.php
+++ app/Http/Services/ProductRepository.php
@@ -6,14 +6,50 @@ use App\Http\Services\CommentRepository;
 class ProductRepository{
     
     public function allProducts(){
-        return \App\Evaluation_Product::join('users', 'evaluation_product.user_id', '=', 'users.id')
+        $products = \App\Product::all();
+
+        foreach ($products as $key => $product) {
+            $evaluation_product = \App\Product::join('evaluation_product', 'evaluation_product.product_id', '=', 'products.id')
+                                   ->join('evaluations', 'evaluation_product.evaluation_id', '=', 'evaluations.id')
+                                   ->select('products.*', 'evaluation_product.*', 'evaluations.*') 
+                                   ->where('products.id', $product->id)->get();
+            $evaluationArray = [];
+            foreach ($evaluation_product as $value) {
+                $valueEval = 0;
+                if($value->key == 'ONE') 
+                    $valueEval = 1;
+                else if($value->key == 'TWO') 
+                    $valueEval = 2;
+                else if($value->key == 'THREE')
+                    $valueEval = 3;
+                else if($value->key == 'FOUR')
+                    $valueEval = 4;
+                else if($value->key == 'FIVE')
+                    $valueEval = 5;
+
+                $evaluationArray[] = $valueEval;
+            }
+
+            if(count($evaluationArray) >= 2){
+                $mediaEval = array_sum($evaluationArray) / 2;
+            } else {
+                $mediaEval = array_sum($evaluationArray);
+            }
+
+            
+
+            dd($mediaEval);
+            die;
+        }
+
+        
+        return \App\Product::join('users', 'evaluation_product.user_id', '=', 'users.id')
                                ->join('evaluations', 'evaluation_product.evaluation_id', '=', 'evaluations.id')
                                ->join('products', 'evaluation_product.product_id', '=', 'products.id')
                                ->join('comments', 'evaluation_product.comment_id', '=', 'comments.id')
                                ->where('evaluation_product.id', '>', '0')->paginate(7);
 
-        //dd($a);
-        //die;
+        
     }
 
     public function oneProduct($productId){
